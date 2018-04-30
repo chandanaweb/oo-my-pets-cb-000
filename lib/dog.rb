7f67a3c54886d8911775c4e468f9d406c5e600b3
chandana M <chandu11288@gmail.com>
2018-04-30 18:03:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 class Dog
-attr_accessor :mood, :name
-protected :name
+attr_accessor :mood
+
 def initialize(name, mood = "nervous")
   @name = name
   @mood = mood
+end
+
+def name
+  @name
 end
 
 end
